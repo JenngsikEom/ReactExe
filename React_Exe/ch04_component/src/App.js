2beead054ccca327a4d9278fdd84896ae28f3411
JenngsikEom <105258488+JenngsikEom@users.noreply.github.com>
2024-05-17 17:24:18 +0900

    컴포넌트

--- React_Exe/ch04_component/src/App.js
+++ React_Exe/ch04_component/src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+
 // import MyComponent from "./ex06_default_props copy/MyComponent";
 // import Child from "./ex01_function/Child";
 // import Child from "./ex02_class/Child";
@@ -7,8 +8,17 @@ import "./App.css";
 // import Library from "./ex04_props/Library";
 // import MyComponent from "./ex05_props/MyComponent";
 // import MyComponent from "./ex06_default_props/MyComponent";
+// import MyComponent from "./ex07_children/MyComponent";
+// import MyComponent from "./ex08_destructing/MyComponent";
+// import MyComponent from "./ex09_prop_types/MyComponent";
+// import MyComponnt from "./ex10_prop_required/MyComponent";
+// import MyComponent from "./ex11_prop_class/MyComponent";
+// import MyComponent from "./ex12_prop_class/MyComponent";
+// import Team from "./ex13_compose_component/Team";
+// import CommentList from "./ex18_comment/CommentList";
+// import CommentList from "./ex19_comment/CommentList";
 
-import MyComponent from "./ex07_children/MyComponent";
+import CommentList from "./ex20_comment/CommentList";
 
 function App() {
   return (
@@ -19,7 +29,19 @@ function App() {
     //   <MyComponent name="리액트" />
     // </>
 
-    <MyComponent>리액트 프로그래밍</MyComponent>
+    // <MyComponent name="Happy Happy">리액트 프로그래밍</MyComponent>
+
+    // <MyComponent name="행운">리액트</MyComponent>
+    // <MyComponent name={3}>리액트</MyComponent>
+
+    // <MyComponent name="React_Programming">리액트</MyComponent>
+    // <MyComponent name="리액트_뷰_앵귤러_노드_넥스트" favoriteNumber={3}>
+    //   리액트
+    // </MyComponent>
+
+    // <Team title="재능 기부 교류 서비스" />
+
+    <CommentList />
   );
 }
 
